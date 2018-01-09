238e99fcc6674ae0f87e9a13eefbaec5a6f25ecd
Min Kim <mk4555@nyu.edu>
2018-01-09 22:16:44 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,6 +12,11 @@ class Student
  end
 
  def self.create_table
-   
+   sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS students(
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade INTEGER
+      )
  end
 end
