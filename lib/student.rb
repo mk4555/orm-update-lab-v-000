0b83b4fd11138ef1e67b33e11c47eb82b78c5f6e
Min Kim <mk4555@nyu.edu>
2018-01-09 22:14:33 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,7 +5,7 @@ class Student
   attr_reader :id
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
- def initialize
+ def initialize(name, grade)
 
  end
 
